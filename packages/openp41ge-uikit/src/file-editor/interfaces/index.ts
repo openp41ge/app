f489367d6cbffa7323aee22ac885701928d15a84
Richard Kielty <rich.kielty@gmail.com>
2026-07-29 08:22:44 +0100

    chore: remove extracted file-editor engine files from uikit
    All 82 engine files (controllers/, cursor/, input/, interfaces/, model/,
    rendering/, services/, themes/, view/, events.ts, global.d.ts) are now
    in openp41ge-editor-engine. Only the index.ts re-export barrel remains
    in uikit's src/file-editor/.

--- packages/openp41ge-uikit/src/file-editor/interfaces/index.ts
+++
@@ -1,8 +0,0 @@
-export type { IFileReader, IFileWriter, FileReadResult } from "./file-service";
-export type { ISyntaxHighlighter } from "./syntax-highlighter";
-export type { IGrammar, IGrammarRegistry } from "./grammar-registry";
-export type { IFormatter, IFormatterRegistry } from "./formatter-registry";
-export type { IUndoManager } from "./undo-manager";
-export type { ICursorTracker } from "./cursor-tracker";
-export type { IStatusBar } from "./status-bar";
-export type { IAutoScrollController, IScrollController } from "./scroll-controllers";
--- packages/openp41ge-uikit/src/file-editor/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { ExtensionGrammarRegistry } from "./extension-grammar-registry";
-export { ExtensionFormatterRegistry } from "./extension-formatter-registry";
-export { registerBuiltinFormatters } from "./formatters";
