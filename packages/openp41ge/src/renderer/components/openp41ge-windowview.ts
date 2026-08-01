11aaeeace3623abbc8ee1de68883e342cd9d5f29
Richard Kielty <rich.kielty@gmail.com>
2026-08-01 01:32:22 +0100

    feat: folder icon before workspace ID in bottom bar

--- packages/openp41ge/src/renderer/components/openp41ge-windowview.ts
+++ packages/openp41ge/src/renderer/components/openp41ge-windowview.ts
@@ -310,7 +310,7 @@ class Openp41geWindowView extends LitElement {
             title="Open workspace settings"
           >
             ${workspaceFileService.activeData
-              ? html`<span style="font-family:monospace">${workspaceFileService.activeData.id.slice(0, 8)}</span>`
+              ? html`<svg width="14" height="14" viewBox="0 -960 960 960" fill="currentColor" style="margin-right:4px"><path d="M160-240v-480 520-40Zm0 80q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v200h-80v-200H447l-80-80H160v480h200v80H160ZM584-56 440-200l144-144 56 57-87 87 87 87-56 57Zm192 0-56-57 87-87-87-87 56-57 144 144L776-56Z"/></svg><span style="font-family:monospace">${workspaceFileService.activeData.id.slice(0, 8)}</span>`
               : html`<span>open workspace</span>`}
           </div>
         </div>
